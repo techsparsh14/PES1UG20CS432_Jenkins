080e191540991c1e9686f1964e3ada667529bb78
techsparsh14 <79414548+techsparsh14@users.noreply.github.com>
2023-02-15 09:25:35 +0530

    Update hello.cpp
    Fixed error

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << I am Sparsh Agarwal :) PES1UG20CS432\n";
+  cout << "I am Sparsh Agarwal :) PES1UG20CS432\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
